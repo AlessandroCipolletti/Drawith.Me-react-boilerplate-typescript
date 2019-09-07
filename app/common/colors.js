478b936f8c86230565cd206e3510595f6bb41647
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2019-09-07 18:47:44 +0200

    Create colors.js

---
+++ app/common/colors.js
@@ -0,0 +1,86 @@
+// export const gray = [
+//   '#202020',  // dark
+//   '#414141',
+//   '#5a5a5a',
+//   '#777777',  // default
+//   '#929292',
+//   '#adadad',
+//   '#c9c9c9',  // light
+// ]
+
+
+export const gray = [
+  '#22252a',
+  '#353a3f',
+  '#4a5056',
+  '#878e95',
+  '#aeb5bc',
+  '#cfd4d9',
+  '#dfe2e6',
+  '#e9ecef',
+  '#f1f3f5',
+  '#f8f9fa',
+]
+
+
+export const orange = [
+  '#661c00',  // dark
+  '#992900',
+  '#cc3700',
+  '#ff4500',  // default
+  '#ff6a33',
+  '#ff8f66',
+  '#ffb599',  // light
+]
+
+export const red = [
+  '#5a1e1b',  // dark
+  '#7d2621',
+  '#ab3029',
+  '#cb4039',  // default
+  '#d56b69',
+  '#ebadac',
+  '#f6e6e6',  // light
+]
+
+export const yellow = [
+  '#59481d',  // dark
+  '#886d2f',
+  '#c5a550',
+  '#efca74',  // default
+  '#f7e2a7',
+  '#fcf3da',
+  '#fffcf5',  // light
+]
+
+export const green = [
+  '#27503b',  // dark
+  '#377547',
+  '#4b9a5f',
+  '#60bd79',  // default
+  '#8bd6a3',
+  '#b6ebc4',
+  '#e7fbed',  // light
+]
+
+export const blue = [
+  '#263d52',  // dark
+  '#24496e',
+  '#33689d',
+  '#4483c2',  // default
+  '#6fa1d3',
+  '#b1d3f2',
+  '#f0f8fe',  // light
+]
+
+export const rose = [
+  '#204444',  // dark
+  '#30635e',
+  '#488f87',
+  '#5baba3',  // default
+  '#86d4d2',
+  '#b5ecea',
+  '#ebfefe',  // light
+]
+
+export const white = '#ffffff'
