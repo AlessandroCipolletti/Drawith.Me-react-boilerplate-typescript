a296e75a3f1ca86754f66d6e07bf161330b02408
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2019-09-09 02:55:41 +0200

    Fix pages TU

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ import '!file-loader?name=[name].[ext]!./images/favicon.ico'
 import 'file-loader?name=.htaccess!./.htaccess'
 /* eslint-enable import/no-unresolved, import/extensions */
 
-import configureStore from './utils/configureStore'
+import configureStore from 'utils/configureStore'
 
 // Import i18n messages
 import { translationMessages } from './utils/i18n'
--- app/pages/Folder/tests/index.test.js
+++ app/pages/Folder/tests/index.test.js
@@ -8,19 +8,34 @@
 
 import React from 'react'
 import { render } from 'react-testing-library'
+import { Provider } from 'react-redux'
 import { IntlProvider } from 'react-intl'
 // import 'jest-dom/extend-expect' // add some helpful assertions
+
+// Import de Theme Provider
+import { ThemeProvider } from 'styled-components'
+import Theme from 'common/theme'
+
+import history from 'utils/history'
+import configureStore from 'utils/configureStore'
 
 import { Folder } from '../index'
 import { DEFAULT_LOCALE } from '../../../utils/i18n'
 
 describe('<Folder />', () => {
+  const store = configureStore({}, history)
+
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error')
     const dispatch = jest.fn()
+
     render(
       <IntlProvider locale={DEFAULT_LOCALE}>
-        <Folder dispatch={dispatch} />
+        <Provider store={store}>
+          <ThemeProvider theme={Theme}>
+            <Folder dispatch={dispatch} />
+          </ThemeProvider>
+        </Provider>
       </IntlProvider>,
     )
     expect(spy).not.toHaveBeenCalled()
@@ -36,7 +51,11 @@ describe('<Folder />', () => {
       container: { firstChild },
     } = render(
       <IntlProvider locale={DEFAULT_LOCALE}>
-        <Folder />
+        <Provider store={store}>
+          <ThemeProvider theme={Theme}>
+            <Folder />
+          </ThemeProvider>
+        </Provider>
       </IntlProvider>,
     )
     expect(firstChild).toMatchSnapshot()
